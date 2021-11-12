6803323e9cee81fb146e1fcd8869b3c61e423467
Toon Geens <toon.geens@xenit.eu>
2021-11-12 11:04:25 +0100

    Cleanup obsolete code-writer

--- scribe-generator/src/main/java/eu/xenit/contentcloud/scribe/generator/repository/RepositoriesGenerationConfiguration.java
+++ scribe-generator/src/main/java/eu/xenit/contentcloud/scribe/generator/repository/RepositoriesGenerationConfiguration.java
@@ -2,12 +2,8 @@ package eu.xenit.contentcloud.scribe.generator.repository;
 
 import eu.xenit.contentcloud.scribe.generator.ScribeProjectDescription;
 import eu.xenit.contentcloud.scribe.generator.entitymodel.EntityModel;
-import eu.xenit.contentcloud.scribe.generator.properties.ApplicationProperties;
 import eu.xenit.contentcloud.scribe.generator.properties.ApplicationPropertiesCustomizer;
 import io.spring.initializr.generator.io.IndentingWriterFactory;
-import io.spring.initializr.generator.language.java.JavaSourceCode;
-import io.spring.initializr.generator.language.java.JavaSourceCodeWriter;
-import io.spring.initializr.generator.language.java.ScribeJavaSourceCodeWriter;
 import io.spring.initializr.generator.project.ProjectGenerationConfiguration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
