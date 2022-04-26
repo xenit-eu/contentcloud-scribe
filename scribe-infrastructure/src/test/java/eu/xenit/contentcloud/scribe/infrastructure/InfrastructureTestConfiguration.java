8f65013cd813057d56c926ccdb6e47ee0f935b02
Toon Geens <toon.geens@xenit.eu>
2022-04-26 16:52:05 +0200

    Add failing test

---
+++ scribe-infrastructure/src/test/java/eu/xenit/contentcloud/scribe/infrastructure/InfrastructureTestConfiguration.java
@@ -0,0 +1,9 @@
+package eu.xenit.contentcloud.scribe.infrastructure;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+public class InfrastructureTestConfiguration {
+
+}
---
+++ scribe-infrastructure/src/test/java/eu/xenit/contentcloud/scribe/infrastructure/changeset/ChangesetModelTest.java
@@ -0,0 +1,32 @@
+package eu.xenit.contentcloud.scribe.infrastructure.changeset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.hateoas.EntityModel;
+
+@JsonTest
+class ChangesetModelTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void canParseWithJackson() throws IOException {
+
+        assertThat(objectMapper).isNotNull();
+
+        var changesetLink = new ClassPathResource("fixtures/changeset-sample.json").getURL();
+        var typeReference = new TypeReference<EntityModel<ChangesetModel>>() {};
+        var changeset = objectMapper.readValue(changesetLink, typeReference);
+
+        assertThat(changeset).isNotNull();
+    }
+
+}
