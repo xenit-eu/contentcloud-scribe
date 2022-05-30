026b8bc08d3a93dcfa08b58ede7e137e9bb59bf8
eddynmn <71167581+eddynmn@users.noreply.github.com>
2022-05-30 14:46:17 +0200

    openapi

--- scribe-generator/src/main/java/eu/xenit/contentcloud/scribe/generator/openapi/OpenApiDataTypes.java
+++ scribe-generator/src/main/java/eu/xenit/contentcloud/scribe/generator/openapi/OpenApiDataTypes.java
@@ -3,6 +3,6 @@ package eu.xenit.contentcloud.scribe.generator.openapi;
 public class OpenApiDataTypes {
 
     public static final OpenApiDataType STRING = new OpenApiDataType("string");
-    public static final OpenApiDataType INTEGER= new OpenApiDataType("integer");
+    public static final OpenApiDataType INTEGER = new OpenApiDataType("integer");
 
 }
